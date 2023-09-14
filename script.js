57c012f35d7029db4a727ad923bd3e664f1c6295
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2023-09-14 13:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here
 
 function separateNames(fullName) {
-	const names = fullName.split(/\s+\b(?:a|an|the)\b\s+/i);
+	const names = fullName.split(/\s+\b(?:a|an|the)?\b\s+/i);
 		return names;
 }
 const input = 'sandy is a G';
