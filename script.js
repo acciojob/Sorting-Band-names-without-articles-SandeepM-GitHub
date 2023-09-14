bb85a18957d28a782de063cbd5ce78393385ccc2
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2023-09-14 13:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-//your code here
+// Define an array of band names
+let bandNames = [['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-function separateNames(fullName) {
-	const names = fullName.split(/\s+\b(?:a|an|the)?\b\s+/i);
-		return names;
+// Function to remove articles from the beginning of a string
+function removeArticles(name) {
+  return name.replace(/^(the|an|a)\s+/i, ''); // Case-insensitive removal of articles
 }
-const input = 'sandy is a G';
 
-const op = separateNames(input);
+// Remove articles and sort the band names
+bandNames = bandNames.map(removeArticles).sort();
 
-console.log(op);
+// Create a ul element
+const ulElement = document.createElement('ul');
+
+// Loop through the sorted band names and create li elements
+bandNames.forEach(function (bandName) {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
+
+// Add the ul element to the HTML document with id 'band'
+document.getElementById('band').appendChild(ulElement);
